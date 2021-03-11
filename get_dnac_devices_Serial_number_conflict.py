cae7cbd06bbcda769950f4a3c4299ecb6ee7ad25
Richard T Lucht <richardtlucht@gmail.com>
2021-03-11 14:59:11 -0600

    Add files via upload

---
+++ get_dnac_devices_Serial_number_conflict.py
@@ -0,0 +1,50 @@
+#! /usr/bin/env python3
+
+from DNA_ATH import dnac
+import json
+import requests
+import urllib3
+from requests.auth import HTTPBasicAuth
+from prettytable import PrettyTable
+
+
+
+
+
+# Silence the insecure warning due to SSL Certificate
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+def dnac_login(host, username, password):
+    url = "https://{}/api/system/v1/auth/token".format(dnac['host'])
+    token = requests.request("POST", url, auth=HTTPBasicAuth(username, password),
+                                verify=False)
+    data = token.json()
+    return data['Token']
+
+
+
+def network_device(dnac, token):
+
+    response = requests.request("GET", "https://{}/dna/intent/api/v1/network-device?errorCode=SERIAL-NUMBER-CONFLICT".format(dnac['host']), 
+ 
+        headers={
+            'X-Auth-Token': '{}'.format(token),
+            'Content-type': 'application/json'
+        },
+        verify=False
+    )
+
+    data = response.json()
+    
+    for x in range(0,25):
+    #print(json.dumps(data, sort_keys=True, indent=4, separators=(',', ':')))
+        print(data["response"][x]["managementIpAddress"] +'    has confilcting Serial numbers in DNA Center')
+
+
+
+
+login = dnac_login(dnac["host"], dnac["username"], dnac["password"])
+network_device(dnac, login)
+
+#print(dnac_devices)
